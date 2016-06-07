5ff2c811cf52ff39ac30b4f3f48de8caaf07ee88
Shahin2321 <tweak.sara@gmail.com>
2016-06-07 20:26:58 +0430

    some little change

---
+++ SFE/SFE.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class SFE
+{
+    public byte[] Encrypt(byte[] DATA)
+    {
+        byte[] newByte = new byte[DATA.Length];
+        for (int i = 0; i < DATA.Length; i++)
+        {
+            newByte[i] = (byte)(DATA[i] + 2);
+        }
+        return newByte;
+    }
+
+    public byte[] Decrypt(byte[] DATA)
+    {
+        byte[] newByte = new byte[DATA.Length];
+        for (int i = 0; i < DATA.Length; i++)
+        {
+            newByte[i] = (byte)(DATA[i] - 2);
+        }
+        return newByte;
+    }
+
+    public byte[] GetBytes(string PATH)
+    {
+        FileStream fs = new FileStream(PATH, FileMode.Open);
+        byte[] b = new byte[fs.Length];
+        fs.Read(b, 0, b.Length);
+        fs.Close();
+        return b;
+    }
+
+    public bool SaveFile(string PATH, byte[] DATA)
+    {
+        try
+        {
+            FileStream fs = new FileStream(path, FileMode.Create);
+            fs.Write(DATA, 0, DATA.Length);
+            fs.Close();
+            return true;
+        }catch(Exception ex)
+        {
+            return false;
+        }
+    }
+
+}
